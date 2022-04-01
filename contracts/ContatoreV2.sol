afee24829bee5b1459268ad83c6a0f42f3490fae
alessandro <alessandro.copler@gmail.com>
2022-04-01 16:56:00 +0200

    Corretto nome contatore versione 2

--- contracts/ContatoreV2.sol
+++ contracts/ContatoreV2.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract Contatore is Initializable {
+contract ContatoreV2 is Initializable {
     uint256 public x;
 
     function initialize(uint256 _x) public initializer {
