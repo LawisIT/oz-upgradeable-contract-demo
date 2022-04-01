c4956d72037947feb6d115a4e34da407f8b4243e
meni <lcalomeni@gmail.com>
2022-04-01 16:21:16 +0200

    set default network and chainId

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,4 @@
 require("@nomiclabs/hardhat-waffle");
-
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -17,5 +16,24 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    }, 
+//  <NETWORK>: {
+//    url: `${process.env.url}`,
+//    accounts: [process.env.privateKey]
+//  }
+
+  },
+  solidity: {
+      version: "0.8.4",
+      settings: {
+        optimizer: {
+	  enabled: true,
+	  runs: 200
+        }
+      }
+   }
 };
