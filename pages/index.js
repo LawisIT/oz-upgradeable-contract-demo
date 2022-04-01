6c5fee116546932c89f132f809aa0aebbd89495b
meni <lcalomeni@gmail.com>
2022-04-01 17:26:59 +0200

    add ui

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,40 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import React, { useState, useEffect } from "react";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { ethers } from "ethers";
+
+import { contatoreAddress } from "../contatoreConfig";
+
+import Contatore from "../artifacts/contracts/Contatore.sol/Contatore.json";
 
 export default function Home() {
+  const [contract, setContract] = useState();
+
+  useEffect(() => onLoad(), []);
+
+  const onLoad = () => {
+    getContract();
+  };
+
+  const getContract = async () => {
+    if (typeof window.ethereum === "undefined") return;
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    setContract(new ethers.Contract(contatoreAddress, Contatore.abi, signer));
+    console.log("Add Contract: ", contract)
+  };
+
+  const onClick = async () => {
+    try{
+      let transaction = await contract.increaseValue();
+      await transaction.wait();
+      console.log("Increased!")
+    }catch(err){
+      console.log("Error:",err)
+    }
+  }
+
+  if (contract === undefined) return "Loading...";
   return (
     <div className={styles.container}>
       <Head>
@@ -13,57 +45,21 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to OZ Upgradeable Contract
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <div className={styles.grid}>
+          Counter: {contract.x}
+        </div>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <button onClick={onClick}> + increase </button>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        Made with love by <strong>Lawis SRL</strong>
       </footer>
     </div>
-  )
+  );
 }
--- scripts/deployContatoreV1.js
+++ scripts/deployContatoreV1.js
@@ -4,12 +4,23 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 const { ethers, upgrades } = require("hardhat");
+const fs = require("fs");
 
 async function main() {
   // We get the contract to deploy
   const Contatore = await ethers.getContractFactory("Contatore");
-  const proxyContatore = await upgrades.deployProxy(Contatore,[10],{inizializer: "inizialize"});
+  const proxyContatore = await upgrades.deployProxy(Contatore, [10], {
+    inizializer: "inizialize",
+  });
   await proxyContatore.deployed();
+
+  fs.writeFileSync(
+    "./contatoreConfig.js",
+    `
+  export const contatoreAddress = "${proxyContatore.address}"
+  `
+  );
+
   console.log("Box deployed to:", proxyContatore.address);
 }
 
